b7ced138167ec0dbcbad709203bb3008d9b8230e
Knika Dass <knikadass@gmail.com>
2020-08-14 03:00:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,16 +1,20 @@
+require 'pry'
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
 
 class GetRequester
 
+    attr_accessor :url
     
     URL = "URL"
 
-    def initialize(URL)
-        @URL = URL
+    def initialize(url)
+        @url = url
     end
 
+    def get_response_body
+    end
 
-end
+    def parse_json
+    end
