25447a912b6a07e40d75751df0478a9bdfe05380
Knika Dass <knikadass@gmail.com>
2020-08-14 02:58:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,2 +1,19 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+ 
 class GetRequester
+ 
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+ 
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+ 
 end
+ 
+programs = GetPrograms.new.get_programs
+puts programs
+ 
